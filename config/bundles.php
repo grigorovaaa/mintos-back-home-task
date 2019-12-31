3747a397ca5c16e98ee9950e98c2e6ec3c17cca8
grigorovaaa <grigorovaaaa@gmail.com>
2019-12-31 17:02:33 +0300

    add token refresh

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
